7c9320a563633a521532876ce4977ab65e74e57e
Ravini <ravinikuruppu@gmail.com>
2023-10-24 15:24:20 +0530

    inquiry form

--- src/components/others/InquiryForm.jsx
+++ src/components/others/InquiryForm.jsx
@@ -9,22 +9,8 @@ const InquiryForm = () => {
         <MyCardHeader>Create Inquiries</MyCardHeader>
         <MyCardBody>
           <Form>
-          <Form.Label className="form-subtitle"> Personal Information</Form.Label>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Name</Form.Label>
-        <Form.Text className="text-muted">
-        *identifier for the user
-        </Form.Text>
-        <Form.Control type="text" placeholder="e.g. full name" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicID">
-        <Form.Label>ID number</Form.Label>
-        <Form.Text className="text-muted">
-        *identify card number of the User
-        </Form.Text>
-        <Form.Control type="text" placeholder="e.g. ST-20024" />
-      </Form.Group>
+          <Form.Label className="form-subtitle"> Inquiry Information</Form.Label>
+      
    
       <Form.Group className="mb-3" controlId="formBasicSubject">
         <Form.Label>Subject</Form.Label>
@@ -42,13 +28,6 @@ const InquiryForm = () => {
         <Form.Control size="sm" as="textarea" rows={8} />
       </Form.Group>
 
-      <Form.Group controlId="formFileMultiple" className="mb-3">
-      <Form.Label>Inquiry Documents</Form.Label>
-        <Form.Text className="text-muted">
-        *documents related to the inquiry
-        </Form.Text>
-        <Form.Control type="file" multiple size="sm" style={{ padding: '0.05rem 0.3rem 0.2rem 0.3rem' }}/>
-      </Form.Group>
       
 
       
