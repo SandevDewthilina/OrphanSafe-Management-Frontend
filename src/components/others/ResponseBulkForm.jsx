74c2be18f0091feba8f140d31db02c796a51d350
Ravini <ravinikuruppu@gmail.com>
2023-10-26 06:27:57 +0530

    Bulk response front end

--- src/components/others/ResponseBulkForm.jsx
+++ src/components/others/ResponseBulkForm.jsx
@@ -2,21 +2,48 @@ import React, { useState } from 'react';
 import { Col, Form, Row,Button } from 'react-bootstrap';
 import { MyCard, MyCardBody, MyCardHeader } from '../MyCard';
 import Select from "react-select";
-import "../../assets/css/dropdown.css"
+import "../../assets/css/dropdown.css";
+import { useBulkResponseMutation } from "../../slices/adminApiSlice";
+import { useLocation } from 'react-router-dom';
+import { toast } from "react-toastify";
 
 
 
 const ResponseBulkForm = () => {
   const [Subject, setSubject] = useState("");
   const [Description,setDescription]= useState("");
-   
+  const [CreateResponse, { isLoading, isError, isSuccess }] =
+  useBulkResponseMutation();
+
+  const location = useLocation();
+
+  const searchParams = new URLSearchParams(location.search);
+  const selectedValues = searchParams.get('selected');
+  const selectedItems = selectedValues ? selectedValues.split(',') : [];
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await CreateResponse({
+        Subject: Subject,
+        Description: Description,
+        Emails:selectedValues,
+        
+      }).unwrap();
+
+      toast.success(" Create Response completed");
+    } catch (error) {
+      toast.error(error.data.message);
+    }
+  };
+
     return (
       <Row>
         <Col sm={7}>
         <MyCard>
         <MyCardHeader>Bulk Response Form</MyCardHeader>
         <MyCardBody>
-          <Form>
+        <Form onSubmit={submitHandler}>
 
 
 
@@ -25,7 +52,8 @@ const ResponseBulkForm = () => {
         <Form.Text className="text-muted">
         *subject of the response
         </Form.Text>
-        <Form.Control type="text" placeholder="e.g. support request" />
+        <Form.Control type="text" placeholder="e.g. support request" 
+        onChange={(e) => setSubject(e.target.value)}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="caseDescription">
@@ -33,7 +61,8 @@ const ResponseBulkForm = () => {
         <Form.Text className="text-muted">
         *response 
         </Form.Text>
-        <Form.Control size="sm" as="textarea" rows={8} />
+        <Form.Control size="sm" as="textarea" rows={8} 
+        onChange={(e) => setDescription(e.target.value)}/>
       </Form.Group>
 
      
